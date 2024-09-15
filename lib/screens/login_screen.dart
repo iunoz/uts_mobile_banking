71477c30a18e63252c2afd091b42764fab045cfd
panicia16 <panicia535230059@stu.untar.ac.id>
2024-09-15 20:30:31 +0700

    login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,10 +3,17 @@ import 'register_screen.dart';
 import 'home_screen.dart';
 import '../widgets/custom_button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   final bool registrationSuccessful;
 
   const LoginScreen({super.key, this.registrationSuccessful = false});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true; //awal hidden text di set true
 
   @override
   Widget build(BuildContext context) {
@@ -20,36 +27,83 @@ class LoginScreen extends StatelessWidget {
             fontSize: 18,
           ),
         ),
+        centerTitle: true,
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (registrationSuccessful)
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Registration Successful! Please log in.',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (widget.registrationSuccessful)
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 16.0),
+                  child: Text(
+                    'Registration Successful! Please log in.',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              const Text(
+                "Email/Phone Number",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Enter your Email/Phone Number!",
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 192, 248, 214),
+                ),
+              ),
+              const SizedBox(height: 25),
+              const Text(
+                "Password",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  labelText: "Enter your Password!",
+                  filled: true,
+                  fillColor: const Color.fromARGB(255, 192, 248, 214),
+                  suffixIcon: IconButton(
+                    icon: Icon(_obscurePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
                   ),
                 ),
               ),
-            const CustomButton(
-              buttonText: 'Register',
-              destination: RegisterScreen(),
-              color: Colors.greenAccent,
-            ),
-            const CustomButton(
-              buttonText: 'Login',
-              destination: HomeScreen(),
-              color: Colors.greenAccent,
-              usePushReplacement: true,
-            ),
-          ],
+              const SizedBox(height: 25),
+              Center(
+                child: CustomButton(
+                  buttonText: 'LOGIN',
+                  destination: const HomeScreen(),
+                  color: Colors.greenAccent,
+                  usePushReplacement: true,
+                ),
+              ),
+              const SizedBox(height: 25),
+              Center(
+                child: CustomButton(
+                  buttonText: "Don't Have Any Account? Register",
+                  destination: const RegisterScreen(),
+                  color: Colors.greenAccent,
+                  usePushReplacement: false,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
