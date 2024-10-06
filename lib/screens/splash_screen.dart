dd1e75811c1a3cee75329e9b611fce07b7d05ded
Tzuryan <ryanhermanto123z@gmail.com>
2024-10-06 21:27:41 +0700

    splash screen fixed position

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -7,18 +7,24 @@ class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       splash: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Memastikan berada di tengah vertikal
         children: [
-          Center(
-            child: Lottie.asset(
-                'assets/animations/Animation - 1728215250294.json'),
-          )
+          Expanded( // Gunakan Expanded agar widget dapat menyesuaikan ruang yang tersedia
+            child: Center(
+              child: Lottie.asset(
+                'assets/animations/Animation - 1728215250294.json',
+                fit: BoxFit.contain, // Memastikan animasi menyesuaikan ukuran layar
+              ),
+            ),
+          ),
         ],
       ),
       nextScreen: const LoginScreen(),
+      splashIconSize: MediaQuery.of(context).size.height * 0.5, // Ukuran splash sekitar setengah layar
+      backgroundColor: Colors.white, // Warna latar belakang bisa disesuaikan
     );
   }
 }
