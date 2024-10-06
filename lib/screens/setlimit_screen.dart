292b832a31152ef51750ec108b9fb3a4b4c2180d
pqmlw <paquita.535230062@stu.untar.ac.id>
2024-10-06 12:29:50 +0700

    penambahan background, kartu

--- lib/screens/setlimit_screen.dart
+++ lib/screens/setlimit_screen.dart
@@ -157,114 +157,139 @@ class SetLimitPageState extends State<SetLimitScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Set Limit'),
+        title: const Text(
+          "Set Limit",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: const Color(0xFF7EBDA6),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Limit untuk tarik tunai
-              CustomSlider(
-                title: 'Cash Withdrawal Limit',
-                currentLimit: _cashLimit,
-                onChanged: (value) {
-                  setState(() {
-                    _cashLimit = value;
-                    _cashLimitController.text = value.toString();
-                  });
-                },
-              ),
-              TextField(
-                controller: _cashLimitController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Enter Cash Withdrawal Limit',
-                ),
-                onChanged: (value) => _onLimitInputChanged(value, 'cash'),
-                onEditingComplete: () => _onTextFieldEditingComplete('cash'),
-              ),
-              const SizedBox(height: 10),
-              // Limit untuk transfer antar rekening
-              CustomSlider(
-                title: 'SUMI Inter-Account Transfer Limit',
-                currentLimit: _sumiTransferLimit,
-                onChanged: (value) {
-                  setState(() {
-                    _sumiTransferLimit = value;
-                    _sumiTransferLimitController.text = value.toString();
-                  });
-                },
-              ),
-              TextField(
-                controller: _sumiTransferLimitController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Enter SUMI Transfer Limit',
-                ),
-                onChanged: (value) => _onLimitInputChanged(value, 'sumi'),
-                onEditingComplete: () => _onTextFieldEditingComplete('sumi'),
-              ),
-              const SizedBox(height: 10),
-              // Limit untuk transfer antar bank
-              CustomSlider(
-                title: 'Interbank Transfer Limit',
-                currentLimit: _interbankLimit,
-                onChanged: (value) {
-                  setState(() {
-                    _interbankLimit = value;
-                    _interbankLimitController.text = value.toString();
-                  });
-                },
-              ),
-              TextField(
-                controller: _interbankLimitController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Enter Interbank Limit',
-                ),
-                onChanged: (value) => _onLimitInputChanged(value, 'interbank'),
-                onEditingComplete: () =>
-                    _onTextFieldEditingComplete('interbank'),
-              ),
-              const SizedBox(height: 10),
-              // Limit untuk transaksi debit
-              CustomSlider(
-                title: 'Debit Transaction Limit',
-                currentLimit: _debitLimit,
-                onChanged: (value) {
-                  setState(() {
-                    _debitLimit = value;
-                    _debitLimitController.text = value.toString();
-                  });
-                },
-              ),
-              TextField(
-                controller: _debitLimitController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  labelText: 'Enter Debit Transaction Limit',
-                ),
-                onChanged: (value) => _onLimitInputChanged(value, 'debit'),
-                onEditingComplete: () => _onTextFieldEditingComplete('debit'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color(0xFF7EBDA6),
-                ),
-                onPressed: () {
-                  _saveLimits();
-                  _showLimitsDialog();
-                },
-                child: const Text('Set Limits'),
-              ),
-            ],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image:
+                AssetImage('assets/images/bg.jpg'), // Updated background image
+            fit: BoxFit.cover,
           ),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              color:
+                  Colors.white.withOpacity(0.5), // White overlay with opacity
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomSlider(
+                      title: 'Cash Withdrawal Limit',
+                      currentLimit: _cashLimit,
+                      onChanged: (value) {
+                        setState(() {
+                          _cashLimit = value;
+                          _cashLimitController.text = value.toString();
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: _cashLimitController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Cash Withdrawal Limit',
+                      ),
+                      onChanged: (value) => _onLimitInputChanged(value, 'cash'),
+                      onEditingComplete: () =>
+                          _onTextFieldEditingComplete('cash'),
+                    ),
+                    const SizedBox(height: 10),
+                    CustomSlider(
+                      title: 'SUMI Inter-Account Transfer Limit',
+                      currentLimit: _sumiTransferLimit,
+                      onChanged: (value) {
+                        setState(() {
+                          _sumiTransferLimit = value;
+                          _sumiTransferLimitController.text = value.toString();
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: _sumiTransferLimitController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter SUMI Inter-Account Transfer Limit',
+                      ),
+                      onChanged: (value) => _onLimitInputChanged(value, 'sumi'),
+                      onEditingComplete: () =>
+                          _onTextFieldEditingComplete('sumi'),
+                    ),
+                    const SizedBox(height: 10),
+                    CustomSlider(
+                      title: 'Interbank Transfer Limit',
+                      currentLimit: _interbankLimit,
+                      onChanged: (value) {
+                        setState(() {
+                          _interbankLimit = value;
+                          _interbankLimitController.text = value.toString();
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: _interbankLimitController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Interbank Transfer Limit',
+                      ),
+                      onChanged: (value) =>
+                          _onLimitInputChanged(value, 'interbank'),
+                      onEditingComplete: () =>
+                          _onTextFieldEditingComplete('interbank'),
+                    ),
+                    const SizedBox(height: 10),
+                    CustomSlider(
+                      title: 'Debit Transaction Limit',
+                      currentLimit: _debitLimit,
+                      onChanged: (value) {
+                        setState(() {
+                          _debitLimit = value;
+                          _debitLimitController.text = value.toString();
+                        });
+                      },
+                    ),
+                    TextField(
+                      controller: _debitLimitController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Debit Transaction Limit',
+                      ),
+                      onChanged: (value) =>
+                          _onLimitInputChanged(value, 'debit'),
+                      onEditingComplete: () =>
+                          _onTextFieldEditingComplete('debit'),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: const Color(0xFF7EBDA6),
+                      ),
+                      onPressed: () {
+                        _saveLimits();
+                        _showLimitsDialog();
+                      },
+                      child: const Text('Set Limits'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
