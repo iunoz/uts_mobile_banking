c3e60e85c51718ebabc93d11501ba5476ebfde02
iunoz <gregorius.535230114@stu.untar.ac.id>
2024-10-06 19:39:04 +0700

    Register tambah background, container, dan penyesuaian font (tahap3)

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -64,170 +64,253 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
       body: Stack(
         children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/login_screen_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
           SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("Full Name",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Enter your full name!",
-                        filled: true,
-                        fillColor: Color.fromARGB(255, 192, 248, 214),
+              child: Center(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 20.0,
+                  ), //jarak dari sisi layar
+                  padding: const EdgeInsets.all(16.0), //padding internal kotak
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF7F2D6),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                        offset: const Offset(0, 5),
                       ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your full name';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 25),
-                    const Text("Phone Number",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      keyboardType: TextInputType.phone,
-                      decoration: const InputDecoration(
-                        labelText: "Enter your Phone Number!",
-                        filled: true,
-                        fillColor: Color.fromARGB(255, 192, 248, 214),
-                      ),
-                      validator: (value) {
-                        //jika kosong error
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your phone number';
-                        }
-                        //cek jika phone number itu angka
-                        if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
-                          return 'Please enter a valid phone number (digits only)';
-                        }
-                        if (value.length < 11 || value.length > 13) {
-                          return 'Phone number must be between 11 and 13 digits';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 25),
-                    const Text("Email",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        labelText: "Enter your Email!",
-                        filled: true,
-                        fillColor: Color.fromARGB(255, 192, 248, 214),
-                      ),
-                      validator: (value) {
-                        //jika kosong error
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your email';
-                        }
-                        //pengecekan penulisan email
-                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                          return 'Please enter a valid email';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 25),
-                    const Text("Password",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: _obscurePassword,
-                      decoration: InputDecoration(
-                        labelText: "Enter your Password!",
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 192, 248, 214),
-                        suffixIcon: IconButton(
-                          icon: Icon(_obscurePassword
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              _obscurePassword = !_obscurePassword;
-                            });
+                    ],
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Center(
+                          child: Text(
+                            "Full Name",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 97, 97, 97),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: "Enter your full name!",
+                            labelStyle: TextStyle(
+                              fontSize: 14,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your full name';
+                            }
+                            return null;
                           },
                         ),
-                      ),
-                      validator: (value) {
-                        //jika pw kosong
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your password';
-                        }
-                        //panjang gabole kurang dr 6
-                        if (value.length < 6) {
-                          return 'Password must be at least 6 characters';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 25),
-                    const Text("Confirm Password",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 5),
-                    TextFormField(
-                      controller: _confirmPasswordController,
-                      obscureText: _obscureConfirmPassword,
-                      decoration: InputDecoration(
-                        labelText: "Re-enter your Password!",
-                        filled: true,
-                        fillColor: const Color.fromARGB(255, 192, 248, 214),
-                        suffixIcon: IconButton(
-                          icon: Icon(_obscureConfirmPassword
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              _obscureConfirmPassword =
-                                  !_obscureConfirmPassword;
-                            });
+                        const SizedBox(height: 25),
+                        const Center(
+                          child: Text(
+                            "Phone Number",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 97, 97, 97),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          keyboardType: TextInputType.phone,
+                          decoration: const InputDecoration(
+                            labelText: "Enter your Phone Number!",
+                            labelStyle: TextStyle(
+                              fontSize: 14,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            //jika kosong error
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your phone number';
+                            }
+                            //cek jika phone number itu angka
+                            if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                              return 'Please enter a valid phone number (digits only)';
+                            }
+                            if (value.length < 11 || value.length > 13) {
+                              return 'Phone number must be between 11 and 13 digits';
+                            }
+                            return null;
                           },
                         ),
-                      ),
-                      validator: (value) {
-                        //tdk bole kosong confirmpassword
-                        if (value == null || value.isEmpty) {
-                          return 'Please confirm your password';
-                        }
-                        //disamain ama pw
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match';
-                        }
-                        return null;
-                      },
+                        const SizedBox(height: 25),
+                        const Center(
+                          child: Text(
+                            "Email",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 97, 97, 97),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: "Enter your Email!",
+                            labelStyle: TextStyle(
+                              fontSize: 14,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            //jika kosong error
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            //pengecekan penulisan email
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 25),
+                        const Center(
+                          child: Text(
+                            "Password",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 97, 97, 97),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: _obscurePassword,
+                          decoration: InputDecoration(
+                            labelText: "Enter your Password!",
+                            labelStyle: const TextStyle(
+                              fontSize: 14,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            suffixIcon: IconButton(
+                              icon: Icon(_obscurePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                            ),
+                            border: const OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            //jika pw kosong
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            //panjang gabole kurang dr 6
+                            if (value.length < 6) {
+                              return 'Password must be at least 6 characters';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 25),
+                        const Center(
+                          child: Text(
+                            "Confirm Password",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 97, 97, 97),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          controller: _confirmPasswordController,
+                          obscureText: _obscureConfirmPassword,
+                          decoration: InputDecoration(
+                            labelText: "Re-enter your Password!",
+                            labelStyle: const TextStyle(
+                              fontSize: 14,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            suffixIcon: IconButton(
+                              icon: Icon(_obscureConfirmPassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureConfirmPassword =
+                                      !_obscureConfirmPassword;
+                                });
+                              },
+                            ),
+                            border: const OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            //tdk bole kosong confirmpassword
+                            if (value == null || value.isEmpty) {
+                              return 'Please confirm your password';
+                            }
+                            //disamain ama pw
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 25),
+                        Center(
+                          child: CustomButton(
+                            buttonText: 'Register',
+                            destination:
+                                const LoginScreen(), // Set the destination
+                            color: const Color(0xFF7EBDA6),
+                            borderRadius: 18,
+                            textColor: Colors.white,
+                            transitionType: PageTransitionType.slideRight,
+                            customNavigation: () {
+                              if (_formKey.currentState!.validate()) {
+                                //ketika form valid maka handle register jalan
+                                _handleRegister();
+                              }
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 25),
-                    Center(
-                      child: CustomButton(
-                        buttonText: 'Register',
-                        destination: const LoginScreen(), // Set the destination
-                        color: const Color.fromARGB(255, 192, 248, 214),
-                        borderRadius: 18,
-                        transitionType: PageTransitionType.slideRight,
-                        customNavigation: () {
-                          if (_formKey.currentState!.validate()) {
-                            //ketika form valid maka handle register jalan
-                            _handleRegister();
-                          }
-                        },
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
